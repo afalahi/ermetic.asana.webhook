e8988ba182b8a60208e9eea8935c76faf6855f1e
Ali Falahi <ali@afalahi.com>
2022-12-11 12:38:08 -0700

    added ssm and cleaned up code

--- index.ts
+++ index.ts
@@ -1,39 +1,47 @@
 import axios from 'axios';
-import { IFinding } from './IFinding';
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
+
+import { IFinding } from './IFinding';
 
 export const handler = async (
   event: APIGatewayEvent,
   ctx: Context
 ): Promise<APIGatewayProxyResult> => {
+  //Function to return the APIGatewayProxyResult response for cleaner code
+  const response = (
+    message: string | unknown,
+    statusCode: number
+  ): APIGatewayProxyResult => {
+    return {
+      statusCode,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message }),
+    };
+  };
+
   // Return early if body or variables are null
   if (!event.body) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({
-        message: 'The Event body was null',
-      }),
-    };
+    return response('The Event body was null', 404);
   }
   if (!process.env.ASANA_PROJECT_ID || !process.env.ASANA_WORKSPACE_ID) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message:
-          'Please check the project and workspace env variables. Variable empty or null',
-      }),
-    };
+    return response(
+      'Please check the project and workspace env variables. Variable empty or null',
+      400
+    );
   }
   if (!process.env.ASANA_URL) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: 'The env variable ASANA_URL is empty or null',
-      }),
-    };
+    return response('The env variable ASANA_URL is empty or null', 400);
   }
   // Parse the finding from the event
   const finding: IFinding = JSON.parse(event.body);
+
+  //instantiate SSM client and command to get the token from the parameter store
+  const ssmClient = new SSMClient({ region: 'us-west-2' });
+  const ssmCommand = new GetParameterCommand({
+    Name: 'ASANA_TOKEN',
+    WithDecryption: true,
+  });
 
   // Formatting the Finding to html_notes. The new lines and spaces affect the formatting of the task details
   const resources = `<ul>${finding.resources.map(
@@ -57,44 +65,27 @@ export const handler = async (
     },
   };
   try {
+    const asanaToken = await ssmClient.send(ssmCommand);
+
     const res = await axios.post(process.env.ASANA_URL, payload, {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        Authorization: `Bearer ${process.env.ASANA_TOKEN}`,
+        Authorization: `Bearer ${asanaToken.Parameter?.Value}`,
       },
     });
-    return {
-      statusCode: res.status,
-      body: JSON.stringify({
-        message: 'Success',
-      }),
-    };
+    return response('Success', res.status);
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
       if (error.response) {
-        return {
-          statusCode: error.response.status,
-          body: JSON.stringify({
-            message: error.response.data,
-            payload: payload,
-          }),
-        };
+        return response(error.response.data, error.response.status);
       } else {
-        return {
-          statusCode: 500,
-          body: JSON.stringify({
-            message:
-              'Something went wrong, we made a request but no response from Asana',
-          }),
-        };
+        return response(
+          'Something went wrong, we made a request but no response from Asana',
+          500
+        );
       }
     }
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: error,
-      }),
-    };
+    return response(error, 500);
   }
 };
